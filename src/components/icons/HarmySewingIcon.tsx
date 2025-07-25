fae0d857851f73692e317497caa9242ac4684dd2
Harmonie Nankafu <nankafuharmonie@gmail.com>
2025-07-25 14:57:46 +0000

    Fait un bon design de cet icône

--- src/components/icons/HarmySewingIcon.tsx
+++ src/components/icons/HarmySewingIcon.tsx
@@ -2,23 +2,18 @@
 export const HarmySewingIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 256 256"
+      viewBox="0 0 100 100"
       width="1em"
       height="1em"
       {...props}
       fill="currentColor"
     >
-      <rect width="256" height="256" fill="none" />
-      <text
-        x="50%"
-        y="50%"
-        dominantBaseline="central"
-        textAnchor="middle"
-        fontSize="150"
-        fontFamily="Belleza, sans-serif"
-        fontWeight="bold"
-      >
-        HS
-      </text>
+      <g fontFamily="Belleza, sans-serif" fontWeight="bold" fontSize="50">
+        <text x="22" y="70">H</text>
+        <text x="50" y="70">S</text>
+      </g>
+      <style>{`
+        @import url('https://fonts.googleapis.com/css2?family=Belleza&display=swap');
+      `}</style>
     </svg>
   );
