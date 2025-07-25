28fff625a8175dbdfc4ced06178645cc10150ac9
Harmonie Nankafu <nankafuharmonie@gmail.com>
2025-07-25 12:39:03 +0000

    Pas encore

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { SewingPinIcon } from 'lucide-react';
 
 export default function AboutPage() {
   return (
