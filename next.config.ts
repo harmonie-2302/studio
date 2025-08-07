db33f6c47ec91c15102b5f7b191c56302600370f
Harmonie Nankafu <nankafuharmonie@gmail.com>
2025-08-07 19:23:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  serverActions: {
+    bodySizeLimit: '4.5mb', // Default is 1mb
+    // Extend the timeout for all server actions
+    // By default, the timeout is 60 seconds
+    executionTimeout: 120, // 2 minutes
+  },
 };
 
 export default nextConfig;
